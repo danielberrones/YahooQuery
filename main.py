2ef9fd08270f57f341a5e857f773d44532bda130
danielberrones <daniel.a.berrones@gmail.com>
2024-02-22 00:52:10 -0600

    refactored

---
+++ main.py
@@ -0,0 +1,13 @@
+import yahooquery
+import matplotlib.pyplot as plt
+import pandas as pd
+
+symbol = 'tsla'
+
+output = yahooquery.Ticker(symbol)
+close = output.history()['close']
+close = close.reset_index()
+close['date'] = pd.to_datetime(close['date']) 
+plt.figure(figsize=(10, 6))
+plt.plot(close['date'],close['close'],marker='o') 
+plt.show()
